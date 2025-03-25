1271baeb4c99cf63b2a739f264e9bfe117c186a0
redaphid <iam@aaronherres.com>
2025-03-24 21:49:02 -0700

    basic tests seem to work

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { AutoRouter, text, error } from 'itty-router';
 
 export default {
 	async fetch(request, env, ctx) {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
+		// if it's a get, return 'hi'
+		if (request.method === 'GET') {
+			const shortcode = request.url.split('/').pop()
+			// if the url is in the kv, return the url as a permanent redirect
+			const url = await env.URL_MAP.get(shortcode)
+			if (url) return new Response(url, { status: 301 })
+			return new Response('Not found', { status: 404 })
+		}
+		if (request.method === 'POST') {
+			// if the url is not in the kv, add it and return the url.
+			// get the shortcode by parsing the url
+			const shortcode = request.url.split('/').pop()
+			const url = await env.URL_MAP.get(shortcode)
+			if (url) return new Response('Short code already exists', { status: 400 })
+			const data = await request.json()
+			console.log(data)
+			const postBodyUrl = data.url
+			if (!postBodyUrl) return new Response('Missing url', { status: 400 })
+			await env.URL_MAP.put(shortcode, postBodyUrl)
+
+			return new Response(
+				JSON.stringify({ url: request.url.split('/').slice(0, -1).join('/') + '/' + shortcode }),
+				{
+					status: 201,
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+			)
 		}
 	},
 };
